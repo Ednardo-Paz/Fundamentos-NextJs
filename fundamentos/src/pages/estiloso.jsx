a02d1daa3f525389753c3c3fbe32a657afda131e
Ednardo Paz <eds_paz@hotmail.com>
2022-06-11 18:29:17 -0300

    Feat: Navegação entre componentes

--- fundamentos/src/pages/estiloso.jsx
+++ fundamentos/src/pages/estiloso.jsx
@@ -1,7 +1,9 @@
 import styles from '../styles/Estiloso.module.css';
+import Link from 'next/link';
 export default function Estiloso() {
   return (
     <div className={styles.roxo}>
+      <Link href={'/'}>Voltar</Link>
       <h1>Estilo Usando CSS Module</h1>
     </div>
   );
