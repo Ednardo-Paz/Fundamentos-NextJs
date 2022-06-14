204317ad40ea5ef3e658c42e0f05982cc6783001
Ednardo Paz <eds_paz@hotmail.com>
2022-06-13 22:49:14 -0300

    Feat: Component layout

--- fundamentos/src/pages/estiloso.jsx
+++ fundamentos/src/pages/estiloso.jsx
@@ -1,10 +1,12 @@
 import styles from '../styles/Estiloso.module.css';
 import Link from 'next/link';
+import Layout from '../components/Layout';
 export default function Estiloso() {
   return (
-    <div className={styles.roxo}>
-      <Link href={'/'}>Voltar</Link>
-      <h1>Estilo Usando CSS Module</h1>
-    </div>
+    <Layout titulo="Exemplo de css modularizado">
+      <div className={styles.roxo}>
+        <h1>Estilo Usando CSS Module</h1>
+      </div>
+    </Layout>
   );
 }
