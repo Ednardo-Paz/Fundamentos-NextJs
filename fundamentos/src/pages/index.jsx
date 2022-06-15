705cd9fa73fc45f0308f417d92ec51460f05d591
Ednardo Paz <eds_paz@hotmail.com>
2022-06-14 23:51:21 -0300

    Feat: Navegação simples

--- fundamentos/src/pages/index.jsx
+++ fundamentos/src/pages/index.jsx
@@ -12,6 +12,7 @@ export default function Inicio() {
       <Navegador destino={'/estiloso'} texto={'Estiloso'} />
       <Navegador destino={'/exemplo'} texto={'Exemplo'} />
       <Navegador destino={'/jsx'} texto={'Jsx'} cor={'crimson'} />
+      <Navegador destino={'/navegacao'} texto={'Navegação 01'} cor={'green'} />
     </div>
   );
 }
